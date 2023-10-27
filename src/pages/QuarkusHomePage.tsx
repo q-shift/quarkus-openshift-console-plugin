7a1eebad3904e110f10e92e2f8413259cb328cb9
Ioannis Canellos <iocanel@gmail.com>
2023-10-27 21:43:44 +0300

    feat: display the list of running quarkus applications

--- src/pages/QuarkusHomePage.tsx
+++ src/pages/QuarkusHomePage.tsx
@@ -9,9 +9,23 @@ import {
 } from '@patternfly/react-core';
 import './quarkus.css';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+import { consoleFetchJSON } from '@openshift-console/dynamic-plugin-sdk';
 
 export default function QuarkusPage() {
   const { t } = useTranslation('plugin__console-plugin-template');
+
+  const [deployments, setDeploymnets] = useState([]);
+  const [deploymentConfigs, setDeploymnetConfigs] = useState([]);
+  
+  useEffect(() => {
+    consoleFetchJSON('/api/kubernetes/apis/apps/v1/namespaces/ikanello1-dev/deployments').then((res) => {
+        setDeploymnets(res.items);
+    });
+    consoleFetchJSON('/api/kubernetes/apis/apps.openshift.io/v1/namespaces/ikanello1-dev/deploymentconfigs').then((res) => {
+        setDeploymnetConfigs(res.items);
+    });
+  }, []);
 
   return (
     <>
@@ -36,6 +50,20 @@ export default function QuarkusPage() {
               )}
               <code>{t('exposedModules')}</code>{' '}
               {t('in package.json mapping the reference to the module.')}
+              <PageSection variant="light">
+               <Title headingLevel="h1">{t('Quarkus Applications')}</Title>
+              { deployments && deployments
+                      .filter(d => (d.metadata.labels['app.openshift.io/runtime'] === 'quarkus'))
+                      .map(d => 
+                <h1>{d.metadata.name}</h1>
+              )}
+
+              { deploymentConfigs && deploymentConfigs
+                      .filter(d => (d.metadata.labels['app.openshift.io/runtime'] === 'quarkus'))
+                      .map(d => 
+                <h1>{d.metadata.name}</h1>
+              )}
+              </PageSection>
             </Text>
           </TextContent>
         </PageSection>
