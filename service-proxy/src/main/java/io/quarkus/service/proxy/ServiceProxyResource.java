a012167e88d3145df8e1ffd1620af2acc53ce1ba
Ioannis Canellos <iocanel@gmail.com>
2023-12-02 18:51:11 +0200

    chore: improve ServiceProxyResource

--- service-proxy/src/main/java/io/quarkus/service/proxy/ServiceProxyResource.java
+++ service-proxy/src/main/java/io/quarkus/service/proxy/ServiceProxyResource.java
@@ -1,5 +1,7 @@
 package io.quarkus.service.proxy;
 
+import org.jboss.logging.Logger;
+
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.client.WebClient;
@@ -7,10 +9,15 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.Produces;
  
 
-@Path("/service/proxy")
+@Path("/produi")
 public class ServiceProxyResource {
+
+  private Logger log = Logger.getLogger(ServiceProxyResource.class);
+  private static final String PATH = "/q/dev";
 
   @Inject
   KubernetesClient kubernetesClient;
@@ -19,14 +26,18 @@ public class ServiceProxyResource {
   Vertx vertx;
 
   @GET
-  @Path("/{namespace}/{name}")
+  @Path("/{namespace}/{name}/")
+  @Produces(MediaType.TEXT_HTML)
   public Response proxy(String namespace, String name, String path) {
        WebClient client = WebClient.create(vertx);
+       log.info("Proxying: http://" + name + "." + namespace +  PATH);
        return client
-            .get(80, name + "." + namespace, "/q/dev")
+            .get(80, name + "." + namespace, PATH)
             .send()
             .toCompletionStage()
-            .thenApply(response -> Response.status(response.statusCode()).entity(response.bodyAsString()).build())
+            .thenApply(response -> Response.status(response.statusCode())
+            .entity(response.body())
+            .build())
             .toCompletableFuture()
             .join();
   }
