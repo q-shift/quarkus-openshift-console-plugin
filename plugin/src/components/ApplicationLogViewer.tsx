ee241d4264e00fe8700662314b0f5b3db4066b1f
Ioannis Canellos <iocanel@gmail.com>
2024-02-02 21:41:02 +0200

    chore: second draft of log viewer

--- plugin/src/components/ApplicationLogViewer.tsx
+++ plugin/src/components/ApplicationLogViewer.tsx
@@ -1,47 +1,174 @@
 import * as React from 'react';
 import { LogViewer, LogViewerSearch } from '@patternfly/react-log-viewer';
-import { useEffect, useState } from 'react';
+import {
+	Button,
+	Tooltip,
+	Toolbar,
+	ToolbarContent,
+	ToolbarGroup,
+	ToolbarItem,
+	ToolbarToggleGroup
+} from '@patternfly/react-core';
+import OutlinedPlayCircleIcon from '@patternfly/react-icons/dist/esm/icons/outlined-play-circle-icon';
+import ExpandIcon from '@patternfly/react-icons/dist/esm/icons/expand-icon';
+import PauseIcon from '@patternfly/react-icons/dist/esm/icons/pause-icon';
+import PlayIcon from '@patternfly/react-icons/dist/esm/icons/play-icon';
+import EllipsisVIcon from '@patternfly/react-icons/dist/esm/icons/ellipsis-v-icon';
+import DownloadIcon from '@patternfly/react-icons/dist/esm/icons/download-icon';
 import { Application } from '../types';
-import { Toolbar, ToolbarContent, ToolbarItem } from '@patternfly/react-core';
 
-const ApplicationLogViewer: React.FC<{application: Application, containerName?: string }> = ({ application, containerName }) => {
+const ApplicationLogViewer: React.FC<{ application: Application, containerName?: string }> = ({ application, containerName }) => {
+  const [content, setContent] = React.useState([]);
+  const [isPaused, setIsPaused] = React.useState(false);
+  const [isFullScreen] = React.useState(false);
+  const [itemCount, setItemCount] = React.useState(1);
+  const [currentItemCount, setCurrentItemCount] = React.useState(0);
+  const [renderData, setRenderData] = React.useState('');
+  const [timer, setTimer] = React.useState(null);
+  const [buffer, setBuffer] = React.useState([]);
+  const [linesBehind, setLinesBehind] = React.useState(0);
+  const logViewerRef = React.useRef();
 
-  const [lines, setLines] = useState<string[]>(["mic", "check", "one", "two"]);
-  const [content, setContent] = useState<string>('');
-
-  useEffect(() => {
-    console.log("Setting up logs");
-    if (application && application.metadata) {
-    console.log("Setting lines for app: " + application.metadata.name);
-      setLines([
-        'Application:' + application.metadata.name,
-        'Container:' + containerName || application.metadata.name
+  React.useEffect(() => {
+    setContent([
+      'This is a test log entry.',
+      'This is another one.',
+      'And another one',
+      'an yet another one'
       ]);
-    }
+    setTimer(
+      window.setInterval(() => {
+        setItemCount(itemCount => itemCount + 1);
+      }, 500)
+    );
+    return () => {
+      window.clearInterval(timer);
+    };
   }, []);
 
-  useEffect(() => {
-    setContent(lines.join('\n'));
-    console.log("Setting content: " + lines.join('\n'));
-  }, [lines]);
+  React.useEffect(() => {
+    if (itemCount > content.length) {
+      window.clearInterval(timer);
+    } else {
+      setBuffer(content.slice(0, itemCount));
+    }
+  }, [itemCount]);
 
+  React.useEffect(() => {
+    if (!isPaused && buffer.length > 0) {
+      setCurrentItemCount(buffer.length);
+      setRenderData(buffer.join('\n'));
+      if (logViewerRef && logViewerRef.current) {
+       //TODO: Fix the commented line below 
+       // logViewerRef.current.scrollToBottom();
+      }
+    } else if (buffer.length !== currentItemCount) {
+      setLinesBehind(buffer.length - currentItemCount);
+    } else {
+      setLinesBehind(0);
+    }
+  }, [isPaused, buffer]);
+
+  const onExpandClick = _event => {
+  };
+
+  const onDownloadClick = () => {
+    const element = document.createElement('a');
+    const file = new Blob(content, { type: 'text/plain' });
+    element.href = URL.createObjectURL(file);
+    element.download = `logs.txt`;
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
+  const onScroll = ({ scrollOffsetToBottom, _scrollDirection, scrollUpdateWasRequested }) => {
+    if (!scrollUpdateWasRequested) {
+      if (scrollOffsetToBottom > 0) {
+        setIsPaused(true);
+      } else {
+        setIsPaused(false);
+      }
+    }
+  };
+
+  const ControlButton = () => (
+      <Button
+        variant={isPaused ? 'plain' : 'link'}
+        onClick={() => {
+          setIsPaused(!isPaused);
+        }}
+      >
+        {isPaused ? <PlayIcon /> : <PauseIcon />}
+        {isPaused ? ` Resume Log` : ` Pause Log`}
+      </Button>
+    );
+    
+  const leftAlignedToolbarGroup = (
+    <React.Fragment>
+      <ToolbarToggleGroup toggleIcon={<EllipsisVIcon />} breakpoint="md">
+        <ToolbarItem variant="search-filter">
+          <LogViewerSearch onFocus={_e => setIsPaused(true)} placeholder="Search" />
+        </ToolbarItem>
+      </ToolbarToggleGroup>
+      <ToolbarItem>
+        <ControlButton />
+      </ToolbarItem>
+    </React.Fragment>
+  );
+
+  const rightAlignedToolbarGroup = (
+    <React.Fragment>
+      <ToolbarGroup variant="icon-button-group">
+        <ToolbarItem>
+          <Tooltip position="top" content={<div>Download</div>}>
+            <Button onClick={onDownloadClick} variant="plain" aria-label="Download current logs">
+              <DownloadIcon />
+            </Button>
+          </Tooltip>
+        </ToolbarItem>
+        <ToolbarItem>
+          <Tooltip position="top" content={<div>Expand</div>}>
+            <Button onClick={onExpandClick} variant="plain" aria-label="View log viewer in full screen">
+              <ExpandIcon />
+            </Button>
+          </Tooltip>
+        </ToolbarItem>
+      </ToolbarGroup>
+    </React.Fragment>
+  );
+
+  const FooterButton = () => {
+    const handleClick = _e => {
+      setIsPaused(false);
+    };
+    return (
+      <Button onClick={handleClick} isBlock>
+        <OutlinedPlayCircleIcon />
+        resume {linesBehind === 0 ? null : `and show ${linesBehind} lines`}
+      </Button>
+    );
+  };
   return (
-    <>
-      <LogViewer
-        height="300"
-        data={content}
-        theme="dark" 
-        toolbar={
+    <LogViewer
+      data={renderData}
+      id="application-log-viewer"
+      scrollToRow={currentItemCount}
+      innerRef={logViewerRef}
+      height={isFullScreen ? '100%' : 600}
+      toolbar={
         <Toolbar>
           <ToolbarContent>
-            <ToolbarItem>
-              <LogViewerSearch placeholder="Search" />
-            </ToolbarItem>
+            <ToolbarGroup>{leftAlignedToolbarGroup}</ToolbarGroup>
+            <ToolbarGroup>{rightAlignedToolbarGroup}</ToolbarGroup>
           </ToolbarContent>
         </Toolbar>
-      }/>
-      </>
+      }
+      overScanCount={10}
+      footer={isPaused && <FooterButton />}
+      onScroll={onScroll}
+    />
   );
-}
+};
 
 export default ApplicationLogViewer;
